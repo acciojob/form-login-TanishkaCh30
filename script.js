93d069045c596765fe373a3ac2c4d99f57c0ceab
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-13 10:25:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+let submitBtn = document.getElementById("btn");
+
+
 function getFormvalue() {
-    //Write your code here
+let firstName = document.querySelector('input[name="fname"]');
+let fName = firstName.value.trim();
+let lastName = document.querySelector('input[name="lname"]');
+let lName = lastName.value.trim();
+
+   alert(`${fName} ${lName}`); 
 
 }
+
+
+submitBtn.addEventListener("click",getFormvalue)
